d89c58de4652ca759712aa6432ba295661a4dbb8
Brian <heinhtetzaw90@gmail.com>
2019-04-27 07:36:51 +0800

    Add Simple hello at root url

---
+++ src/main/java/com/brian/springweb/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.brian.springweb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String hello() {
+        return "Hello";
+    }
+}
